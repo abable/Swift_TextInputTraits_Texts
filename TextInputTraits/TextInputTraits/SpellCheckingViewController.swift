d91a4c9cbcabe0075a70de1b4f54cf6d11718c70
stven1004 <stven100482@gmail.com>
2019-05-29 16:27:24 +0900

    add project

---
+++ TextInputTraits/TextInputTraits/SpellCheckingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SpellCheckingViewController.swift
+//  TextInputTraits
+//
+//  Created by Seungjun Lim on 29/05/2019.
+//  Copyright © 2019 Seungjun Lim. All rights reserved.
+//
+
+import Foundation
